15bdd6f90b1c18f1707da510f288a9ebdea40804
mesteranas <135442710+mesteranas@users.noreply.github.com>
2024-06-16 14:49:31 +0300

    16/06/2024c=3

--- athkar/lib/athkarDialog.dart
+++ athkar/lib/athkarDialog.dart
@@ -1,4 +1,4 @@
-import 'package:audioplayers/audioplayers.dart';
+import 'package:video_player/video_player.dart';
 import 'dart:ffi';
 
 import 'language.dart';
@@ -10,7 +10,7 @@ class AthkarDialog extends StatefulWidget{
   State<AthkarDialog> createState()=>_AthkarDialog(athkars);
 }
 class _AthkarDialog extends State<AthkarDialog>{
-  AudioPlayer audioPlayer=AudioPlayer();
+  var audioPlayer;
   var _=Language.translate;
   bool isPlaying=false;
   bool paused=false;
@@ -22,26 +22,61 @@ class _AthkarDialog extends State<AthkarDialog>{
   _AthkarDialog(this.athkars);
 
   @override
+  void _onVerticalSwipe(DragUpdateDetails details){
+    if (details.delta.dy<0){
+                    index+=1;
+              loadCurrentThekr();
+
+    } else if(details.delta.dy>0){
+                    index-=1;
+              loadCurrentThekr();
+
+    }
+  }
   void initState(){
     super.initState();
     loadCurrentThekr();
     loadAudio();
   }
+
   void loadAudio(){
-    audioPlayer.onPlayerComplete.listen((event) { 
+    try{
+    audioPlayer.addListener(() { 
+      if (audioPlayer.value.position.toString() == audioPlayer.value.duration.toString()){
       isPlaying=false;
+      paused=false;
       setState(() {
         
       });
+    } else {
+            isPlaying=true;
+      setState(() {
+        
+      });
+
+    }
+    
     });
+ } catch(error){
+  print(error);
+
+}
   }
+
   void loadCurrentThekr() {
-    audioPlayer.stop();
+    try{
+    audioPlayer.dispose();
+    } catch(error){
+
+    }
     isPlaying=false;
     var current=athkars[index];
     text=current["text"];
     times=current["times"];
     audio=current["audio"];
+    setState(() {
+      
+    });
   }
   @override
   Widget build(BuildContext context){
@@ -49,11 +84,12 @@ class _AthkarDialog extends State<AthkarDialog>{
       appBar:AppBar(
         title: Text(_("athkar")),
       ) ,
-      body: Center(
+      body: GestureDetector(onVerticalDragUpdate: _onVerticalSwipe,
+      child:Center(
         child: Column(
           children: [
             ListTile(title: Text(text),),
-            ListTile(title: Text(times.toString() + _("times")),),
+            ListTile(title: Text(times.toString() + " "+_("times")),),
             if (index!=0)
                         ElevatedButton(onPressed: (){
               index-=1;
@@ -61,14 +97,17 @@ class _AthkarDialog extends State<AthkarDialog>{
             }, child: Text(_("previous thekr"))),
             if (isPlaying==false)
             ElevatedButton(onPressed: () async{
-              await audioPlayer.play(UrlSource(audio));
+              audioPlayer=VideoPlayerController.networkUrl(Uri.parse(audio))..initialize().then((value) => null);
+              loadAudio();
+              audioPlayer.play();
               isPlaying=true;
+              paused=false;
               setState(() {
                 
               });
             }, child: Text(_("play")))
             else
-            if (paused==false)
+                        if (paused==false)
             ElevatedButton(onPressed: (){
               paused=true;
               audioPlayer.pause();
@@ -79,19 +118,21 @@ class _AthkarDialog extends State<AthkarDialog>{
             else if (paused==true)
             ElevatedButton(onPressed: (){
               paused=false;
-              audioPlayer.resume();
+              audioPlayer.play();
               setState(() {
                 
               });
             }, child: Text(_("resume"))),
+
             if (index!=this.athkars.length-1)
             ElevatedButton(onPressed: (){
               index+=1;
               loadCurrentThekr();
             }, child: Text(_("next thekr"))),
+            
           ],
         ),
       ),
-    );
+    ));
   }
 }
